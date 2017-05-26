ce04c3b0f40d2f81f1229f1052b60c590c5f0a7e
abwhitley <a.blakewhitley@gmail.com>
2017-05-26 16:17:30 -0400

    Shows map with marker and can show user location

--- MapTesting/ViewController.swift
+++ MapTesting/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    var mapView: GMSMapView {
+        return view as! GMSMapView
+    }
+    
+    override func loadView() {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        view = mapView
 
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let locationManager : CLLocationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        mapView.isMyLocationEnabled = true
+        if let myLocation = mapView.myLocation{
+            print("User's location: \(myLocation)")
+            
+        } else {
+            print("User's location is unknown")
+        }
+        
+        
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
+
 
---
+++ Podfile
@@ -0,0 +1,4 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'MapTesting' do
+    pod 'GoogleMaps'
+end`
